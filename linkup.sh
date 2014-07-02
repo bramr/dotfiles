6e5c4ea00962e821171e2a7055b6dc4cf2c19c4c
Bram Rohde <bram@br4m.nl>
2014-07-02 14:51:06 +0200

    small script to create all links with 1 command

---
+++ linkup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s $(pwd)/.vim ~/.vim
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitignore ~/.gitignore
+ln -s $(pwd)/.gitk ~/.gitk
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
