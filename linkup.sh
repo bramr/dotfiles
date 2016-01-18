491a555e963d6ef9878d31753e3142c114974877
Bram Rohde <bram@br4m.nl>
2016-01-18 21:14:59 +0100

    link to new neoconfig location

--- linkup.sh
+++ linkup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir ~/.vim
 ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.vimrc ~/.nvimrc
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.gitignore ~/.gitignore
 ln -s $(pwd)/.gitk ~/.gitk
@@ -9,3 +8,5 @@ ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.bash_profile ~/.bash_profile
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.profile ~/.profile
+mkdir -p ~/.config/nvim
+ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
