5f2e849806638a934919b1831de419d54aba97cb
Bram Rohde <bram@br4m.nl>
2017-06-17 19:17:16 +0200

    Add vrapper config file

--- linkup.sh
+++ linkup.sh
@@ -9,6 +9,7 @@ ln -s $(pwd)/.bash_profile ~/.bash_profile
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.profile ~/.profile
 ln -s $(pwd)/.eslintrc.json ~/.eslintrc.json
+ln -s $(pwd)/.vrapperrc ~/.vrapperrc
 mkdir -p ~/.config/nvim
 mkdir -p ~/Library/Application\ Support/Code/User
 ln -s $(pwd)/config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
