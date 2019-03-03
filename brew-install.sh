39d2aecadc8bdf3f060d1ea7d63eee2903b9a312
Bram Rohde <bram@br4m.nl>
2019-03-03 09:39:54 +0100

    Add clean mac brew install script

--- brew-install.sh
+++ brew-install.sh
@@ -2,14 +2,15 @@
 
 # Install most of my mac packages
 
-brew install tmux nnn vim moreutils atool htop git wget nmap sshfs maven kubernetes-cli watch
+brew install tmux nnn vim moreutils atool htop git wget nmap sshfs maven kubernetes-cli watch cmus mas peco tig ncdu pwsafe ipcalc httpie youtube-dl go python3
 
 brew cask install alfred iterm2 firefox google-chrome visual-studio-code docker vlc spectacle
 brew cask install secure-pipes cyberduck disk-inventory-x typora postman osxfuse sequel-pro java
+brew cask install helium the-unarchiver
 # optional: brew cask install qbittorrent mysqlworkbench duet pluralsight mullvadvpn yujitach-menumeters
 
 brew tap homebrew/cask-versions
-brew install java8
+brew install java8 kotlin
 
 # fonts
 brew tap homebrew/cask-fonts
