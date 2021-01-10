5c26451397132be58b1ade55614476140cd54df7
Bram Rohde <bram@br4m.nl>
2021-01-10 11:02:21 +0100

    Put macos stuff in seperate folder

--- mac/brew-install.sh
+++ mac/brew-install.sh
@@ -2,7 +2,7 @@
 
 # Install most of my mac packages
 
-brew install tmux nnn vim moreutils atool htop git wget nmap sshfs
+brew install tmux vim moreutils atool htop git wget nmap sshfs
 brew install maven kubernetes-cli watch cmus mas peco tig ncdu pwsafe
 brew install ipcalc httpie youtube-dl go python3
 
@@ -17,11 +17,20 @@ brew cask install qbittorrent sonic-pi discord
 brew tap homebrew/cask-versions
 brew install jenv openjdk@11 adoptopenjdk8 adoptopenjdk kotlin
 
-brew cask install eclipse-jee
+brew tap clangen/musikcube
+brew install musikcube
+
+brew install coreutils jq fzf jump bat tree
+
+mkdir -p ~/Developer/Bram
+cd ~/Developer/Bram
+git clone https://github.com/jarun/nnn.git
+cd nnn && make O_NERD=1 && mkdir ~/bin && cp nnn ~/bin/
 
 # fonts
-brew tap homebrew/cask-fonts
-brew install font-hack font-inconsolata
+# brew tap homebrew/cask-fonts
+# brew install font-hack font-inconsolata
+# brew cask install eclipse-jee
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
